2abb18dd10e3afb0b2d87a785119c7d7a3f0fbb2
Segun Alesinloye <segunalesh@gmail.com>
2021-02-05 13:30:54 -0600

    Filtering according to state

--- restaurant/src/Components/navBarComponent.jsx
+++ restaurant/src/Components/navBarComponent.jsx
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 
 class NavBar extends Component {
+
+   
+
     render() { 
         return ( 
             <div>
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
+                <nav onFilter={this.props.handleFilter} class="navbar navbar-expand-lg navbar-light bg-light">
+  {/* <div class="container-fluid"> */}
     <a class="navbar-brand" href="#">Restaurants</a>     
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
+      
+    {/* </div> */}
 </nav>
             </div>
          );
--- restaurant/src/Pages/index.jsx
+++ restaurant/src/Pages/index.jsx
@@ -21,12 +21,49 @@ class Main extends Component {
         })
   );
     }
+
+    handleFilter = (event) => {
+        event.preventDefault()
+        var inputFilter = document.getElementById("input-filter").value
+        console.log("clicked", 
+        inputFilter
+        );
+        const oldResult = this.state.result
+        const newResult = oldResult.filter(c => c.state == inputFilter)
+        this.setState({
+            result: newResult
+        })
+
+    }
+    
+
+    handleSort = (result) => {
+        const results = this.state.result.sort()
+        console.log("sort clicked", results);
+        // for (let i = 0; i < result.length; i++) {
+        //     console.log(result[i].name.sort());            
+        // }
+
+        // const result = this.state.result.sort(function (a,b){
+        //     return a > b
+        // })
+        // console.log(result.sort());
+        // this.setState({
+        //     result
+        // })
+    }
+
     render() { 
         return ( 
             <div>
-                <NavBar/>
+                <NavBar>
+                </NavBar>
             <div className="container">
-            <button className="btn-secondary btn-sm">Sort Alphabetically</button>
+            <form class="d-flex">
+        <input id="input-filter" class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button onClick={this.handleFilter} class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+            <button onClick={() => this.handleSort(this.state.result)} className="btn-secondary btn-sm">Sort Alphabetically</button>
                 <Table result={this.state.result}/>
             </div>
          </div>);
